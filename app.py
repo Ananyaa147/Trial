cd981461ae4af3a438cc126245e8973a7fd33b70
Ananyaa147 <82410042+Ananyaa147@users.noreply.github.com>
2021-04-14 17:55:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/') 
+def home():
+    return render_template('Index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
